af3502fee0e7247c47b32694d4e2ee3e39a3e7ae
fmw666 <fmw19990718@qq.com>
2024-12-18 22:11:18 +0800

    update wechat and chatbot widgets

--- resume-web-api/api/routes/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from . import authentication, resume
-
-
-router = APIRouter()
-router.include_router(authentication.router, tags=["用户认证"], prefix="/auth")
-router.include_router(resume.router, tags=["个人信息"], prefix="/resume")
